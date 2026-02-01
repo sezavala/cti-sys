3abd18e34107ca2e6e145930d18afdcccfeb9374
Sergio Zavala <sezavala@csumb.edu>
2026-01-31 18:26:57 -0800

    fixed endpoint

--- src/gsheet/group_attendance/service.py
+++ src/gsheet/group_attendance/service.py
@@ -101,6 +101,7 @@ def fetch_cti_emails(eng: Engine, cti_ids: List[int]) -> Dict[int, str]:
 
     email_frame = pandas.read_sql(attendance_query, eng)
     for row in email_frame.iterrows():
+        print(row)
         ids_to_email[row.cti_id] = row.email
 
     return ids_to_email
