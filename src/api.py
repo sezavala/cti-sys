1f4dffc8e0897e24d06d1f4d5f2e2d7e8db1c113
Sergio Zavala <sezavala@csumb.edu>
2026-01-31 18:00:10 -0800

    fixed endpoint

--- src/api.py
+++ src/api.py
@@ -12,7 +12,7 @@ from src.students.attendance_entry.router import router as student_attendance_en
 from src.students.withdrawal_processing.router import router as student_withdrawal_router
 
 from src.gsheet.refresh.router import router as gsheet_refresh_router
-from src.gsheet.router import router as gsheet_router
+from src.gsheet.group_attendance.router import router as gsheet_router
 from src.utils.authorization import verify_api_key
 
 api_router = APIRouter(dependencies=[Depends(verify_api_key)])
@@ -87,16 +87,16 @@ api_router.include_router(
     tags=["Students"],
 )
 
-# /api/gsheet/refresh/...
-api_router.include_router(
-    gsheet_refresh_router,
-    prefix="/gsheet/refresh",
-    tags=["GSheet"],
-)
-
 # /api/gsheet/...
 api_router.include_router(
     gsheet_router,
     prefix="/gsheet",
+    tags=["GSheet"],
+)
+
+# /api/gsheet/refresh/...
+api_router.include_router(
+    gsheet_refresh_router,
+    prefix="/gsheet/refresh",
     tags=["GSheet"],
 )
